7d25f8f51360aad3ec43345babba58e486d6f05d
bobole11 <bobolencko24@gmail.com>
2019-01-31 15:16:00 -0500

    Creation de la classse Personne dans le fichier classPerson.py

--- classPerso.py
+++ classPerso.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3.5
 
+class Personne:
+	def __init__(self, *args, **kwargs):
+		self.nom = args[0]
+		self.prenom = args[1]
+		self.age = args[2]
+
+	def __repr__(self):
+		print("Nom: {}, Premom: {} et Age: {}".format(self.nom, self.prenom, self.age))
